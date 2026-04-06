e19c839d4c94a819733585cbec81b32603a11f7c
NullSpider <235911627+IonaraParreira@users.noreply.github.com>
2026-04-06 15:49:57 -0300

    Atualização da Situação

---
+++ temperatura-dos-servidores.py
@@ -0,0 +1,6 @@
+temperatura = float(input("Digite a temperatura atual:"))
+
+if temperatura >25:
+    print("Alerta⚠️  Temperatura acima do limite permitido.Perigo,perigo,perigo.Risco de sobrecarga")
+else:
+    print("Temperatura dentro do limite seguro😎.Não se preocupe,você não vai morrer de calor😏")
